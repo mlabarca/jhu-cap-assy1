1e1ec0b6691dac9089144e302da2e190d8c62ec5
mlabarca <mauriciolabarca4tw@gmail.com>
2017-01-25 18:33:33 -0300

    Add calls to configure Mongoid in application.rb

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,11 @@ module Assignment1
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    Mongoid.load!('./config/mongoid.yml')
+    #which default ORM are we using with scaffold
+    #add  --orm mongoid, or active_record 
+    #    to rails generate cmd line to be specific
+    config.generators {|g| g.orm :active_record}
   end
 end
