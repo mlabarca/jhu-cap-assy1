bb654e9f6fc80882c8e6e023af3eeb8593a1a9d9
mlabarca <mauriciolabarca4tw@gmail.com>
2017-01-29 18:53:47 -0300

    Add main app module, constants, routes.

--- app/assets/javascripts/capstone-scripts.js
+++ app/assets/javascripts/capstone-scripts.js
@@ -4,3 +4,7 @@
 //= require angular
 //= require angular-ui-router
 //= require angular-resource
+
+//= require capstone-scripts/app.module
+//= require capstone-scripts/app.constant
+//= require capstone-scripts/app.routes
---
+++ app/assets/javascripts/capstone-scripts/app.routes.js
@@ -0,0 +1,21 @@
+(function() {
+  "use strict";
+
+  angular
+    .module("app")
+    .config(RouterFunction);
+
+  RouterFunction.$inject = ["$stateProvider",
+                            "$urlRouterProvider", 
+                            "app.APP_CONFIG"];
+
+  function RouterFunction($stateProvider, $urlRouterProvider, APP_CONFIG) {
+    $stateProvider
+    .state("home",{
+      url: "/",
+      templateUrl: APP_CONFIG.main_page_html
+    })
+
+    $urlRouterProvider.otherwise("/");
+  }
+})();
